6558ec23dd5f536c419970a0d90fb3632d30c0ee
Chandan Kumar <chandankr.pra930@gmail.com>
2020-09-16 14:12:45 +0530

    week 34 day 01 assignment M-tube

---
+++ build/precache-manifest.566f7facce624690f8518f2c0edc9531.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7250ad5e3fa004d4fe7eb0dacda5ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f86e7dbc04f43eef9148",
+    "url": "/static/css/main.abd58810.chunk.css"
+  },
+  {
+    "revision": "cd4641ab908f1746ef41",
+    "url": "/static/js/2.3e591748.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3e591748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f86e7dbc04f43eef9148",
+    "url": "/static/js/main.0c3cfe1e.chunk.js"
+  },
+  {
+    "revision": "6a040985a61f21896475",
+    "url": "/static/js/runtime-main.8ccca7a7.js"
+  },
+  {
+    "revision": "37194ebb0d43ab3274bf6760a7e95262",
+    "url": "/static/media/Loading-Image-1.37194ebb.gif"
+  }
+]);
---
+++ src/redux/reducers/playlistReducer.js
@@ -0,0 +1,25 @@
+const initialState = {
+  playlist: null,
+  pending_playlist: [],
+};
+
+export default (state = initialState, action) => {
+  const { payload, type } = action;
+
+  switch (type) {
+    case "FETCH_PLAYLIST":
+      //important
+      return { ...state, playlist: payload.items };
+
+    case "FETCH_PLAYLIST_PENDING":
+      return {
+        ...state,
+        pending_playlist: state.pending_playlist.concat(payload),
+      };
+
+    default:
+      break;
+  }
+
+  return state;
+};
